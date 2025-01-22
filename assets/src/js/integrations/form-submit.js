c615ecc4a988e5f8402cffb84152272e75d9cf60
Dan0sz <18595395+Dan0sz@users.noreply.github.com>
2025-01-22 18:45:00 +0100

    Fixed: this works with most plugins, except CF7.

--- assets/src/js/integrations/form-submit.js
+++ assets/src/js/integrations/form-submit.js
@@ -22,55 +22,11 @@ document.addEventListener('DOMContentLoaded', () => {
 
 			this.forms.forEach((form) => {
 				form.addEventListener('submit', (e) => {
-					if (self.isValid(e.target)) {
+					if (e.target.checkValidity()) {
 						self.trackSubmission();
 					}
 				})
 			})
-		},
-
-		/**
-		 * Runs basic validation on the form.
-		 *
-		 * @param form
-		 * @returns {boolean}
-		 */
-		isValid: function (form) {
-			let self = this;
-			let valid = true;
-
-			Array.from(form).some((element) => {
-				if (self.isInput(element)) {
-					if ((element.tagName === 'INPUT' || element.tagName === 'TEXTAREA') && element.required === true && element.value === '') {
-						valid = false;
-
-						return;
-					}
-
-					/**
-					 * A known caveat in this check is, that if the first option in the SELECT is e.g. "Please make a choice", it will still pass as valid.
-					 */
-					if (element.tagName === 'SELECT' && element.required === true && element.selectedIndex < 0) {
-						valid = false;
-
-						return;
-					}
-				}
-			});
-
-			return valid;
-		},
-
-		/**
-		 * Is element an input field?
-		 *
-		 * @param element
-		 * @returns {boolean}
-		 */
-		isInput: function (element) {
-			let inputs = ['INPUT', 'SELECT', 'TEXTAREA'];
-
-			return inputs.includes(element.tagName);
 		},
 
 		/**
