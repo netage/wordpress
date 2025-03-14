08f583a2876a43177ccbee3bd45a08ac76f89fc7
Dan0sz <18595395+Dan0sz@users.noreply.github.com>
2025-03-14 14:46:24 +0100

    Updated FiltersTest

--- tests/integration/FiltersTest.php
+++ tests/integration/FiltersTest.php
@@ -18,6 +18,15 @@ class FiltersTest extends TestCase {
 
 		$this->assertStringContainsString( 'example.org', $tag );
 		$this->assertStringContainsString( 'plausible.io/api/event', $tag );
+		$this->assertStringContainsString( 'plausible-analytics-js', $tag );
+
+		add_filter( 'plausible_analytics_settings', [ $this, 'enableCompat' ] );
+
+		$class = new Filters();
+		$tag   = $class->add_plausible_attributes( '<script id="plausible-analytics-js" src="test.js">', 'plausible-analytics' );
+
+		remove_filter( 'plausible_analytics_settings', [ $this, 'enableCompat' ] );
+
 		$this->assertStringNotContainsString( 'plausible-analytics-js', $tag );
 	}
 }
